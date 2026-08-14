2439b7b65e33fd6ce5bf293c5fc018878e93ef14
snehab050-techie <snehab050@gmail.com>
2026-08-14 11:10:29 +0530

    Committing changes to files - Exception from one thread won't make other threads stop working - threads work independently

--- Implementing_MultiThreading/src/MultiThreadThing.java
+++ Implementing_MultiThreading/src/MultiThreadThing.java
@@ -11,6 +11,10 @@ public class MultiThreadThing extends Thread{
         for(int i=1; i<=5;i++){
             System.out.println(i+" is printed by thread "+tNo);
 
+            //Exception from one thread won't make other threads stop working - threads work independently
+            if(tNo == 3){
+                throw new RuntimeException();
+            }
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
