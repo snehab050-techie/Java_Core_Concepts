0a993672f18fa6b289a3811e80a543875f94ab2e
snehab050-techie <snehab050@gmail.com>
2026-08-14 11:04:54 +0530

    Committing changes to check the thread number which is responsible for printing values

--- Implementing_MultiThreading/src/MultiThreadThing.java
+++ Implementing_MultiThreading/src/MultiThreadThing.java
@@ -1,9 +1,15 @@
 public class MultiThreadThing extends Thread{
+    int tNo;
+
+    //to check which thread prints value
+    public MultiThreadThing(int threadNO){
+        this.tNo = threadNO;
+    }
 
     @Override
     public void run(){
         for(int i=1; i<=5;i++){
-            System.out.println(i);
+            System.out.println(i+" is printed by thread "+tNo);
 
             try {
                 Thread.sleep(1000);
--- Implementing_MultiThreading/src/MultiThreadingMain.java
+++ Implementing_MultiThreading/src/MultiThreadingMain.java
@@ -1,13 +1,20 @@
 public class MultiThreadingMain {
     static void main() {
-        MultiThreadThing thread1 = new MultiThreadThing();
-        MultiThreadThing thread2 = new MultiThreadThing();
-//        thread1.run();
+//        MultiThreadThing thread1 = new MultiThreadThing();
+//        MultiThreadThing thread2 = new MultiThreadThing();
+////        thread1.run();
+//
+////        thread1.run(); //thread1 will execute first...sequential execution
+////        thread2.run();
+//
+//        thread1.start(); //both thread work simultaneously to print values
+//        thread2.start();
 
-//        thread1.run(); //thread1 will execute first...sequential execution
-//        thread2.run();
-
-        thread1.start(); //both thread work simultaneously to print values
-        thread2.start();
+        //starting 5 threads parallel
+        //All 5 threads will print numbers at the same time
+        for(int i=1;i<=5;i++){
+            MultiThreadThing threads = new MultiThreadThing(i);
+            threads.start();
+        }
     }
 }
