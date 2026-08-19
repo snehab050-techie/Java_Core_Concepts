e2e8f9952dafb18003e1bee44bb4f0a48c4679a9
snehab050-techie <snehab050@gmail.com>
2026-08-19 20:52:25 +0530

    Committing code changes for stream API methods

--- Java_Core/src/Java_8_Features/Employees.java
+++ Java_Core/src/Java_8_Features/Employees.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Employees {
     static void main() {
-        List<String> names = Arrays.asList("Sneha","Janaki","Suhas","Rahul");
+        List<String> names = Arrays.asList("Sneha", "Janaki", "Suhas", "Rahul");
 
         //task is to print employees starting with letter S
         //before Java 8
-        for(String ename : names){
-            if(ename.startsWith("S")){
+        for (String ename : names) {
+            if (ename.startsWith("S")) {
                 System.out.println(ename);
             }
         }
@@ -35,11 +36,83 @@ public class Employees {
                 .filter(n -> n.startsWith("S"))
                 .forEach(System.out::println);
 
-        Collections.sort(names, (a,b)-> a.compareTo(b));
+        Collections.sort(names, (a, b) -> a.compareTo(b));
         System.out.println(names);
 
         //Method referencing
         names.stream().map(name -> name.toUpperCase()).forEach(System.out::println);
         names.stream().map(String::toUpperCase).forEach(System.out::println);
+
+        //Stream API
+        System.out.println("---------Stream API----------");
+        List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30);
+
+        // Without streams - fetch numbers greater than 25
+        for (Integer num : numbers) {
+            if (num > 25) {
+                System.out.println(num);
+            }
+        }
+
+        //with streams - one line
+        numbers.stream()
+                .filter(num -> num > 25)
+                .forEach(System.out::println);
+
+        //filter()
+        List<Integer> res = numbers.stream()
+                .filter(num -> num > 20)
+                .collect(Collectors.toList());
+
+        System.out.println(res);
+
+        //map()
+        List<String> empNames = Arrays.asList("sneha", "suhas", "janu");
+        List<String> ename = empNames.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        System.out.println(ename);
+
+        // sorted() - asc order
+        List<String> lis = empNames.stream()
+                .sorted()
+                .toList();
+        System.out.println(lis);
+
+        //sorted() - desc order
+        empNames.stream()
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
+
+        //distinct()
+        List<Integer> nums =
+                Arrays.asList(10, 20, 10, 30, 20);
+        nums.stream()
+                .distinct()
+                .forEach(System.out::println);
+
+        //limit()
+        nums.stream()
+                .limit(3)
+                .forEach(System.out::println);
+
+        //skip()
+        nums.stream()
+                .skip(2)
+                .forEach(System.out::println);
+
+        //forEach()
+        nums.stream()
+                .forEach(System.out::println);
+
+        //collect()
+        nums.stream()
+                .collect(Collectors.toSet());
+
+        //reduce()
+        int num = nums.stream()
+                .reduce(0, (a, b) -> a + b);
+        System.out.println(num);
     }
 }
