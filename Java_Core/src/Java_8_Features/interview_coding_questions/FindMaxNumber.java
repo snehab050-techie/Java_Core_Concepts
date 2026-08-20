f6a9a41346e51dc0c23a8cf2cd239279fa2be843
snehab050-techie <snehab050@gmail.com>
2026-08-20 23:03:53 +0530

    Alternate methods to find max and min elements in a list

--- Java_Core/src/Java_8_Features/interview_coding_questions/FindMaxNumber.java
+++ Java_Core/src/Java_8_Features/interview_coding_questions/FindMaxNumber.java
@@ -3,6 +3,7 @@ package Java_8_Features.interview_coding_questions;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 //Program to find maximum number
 public class FindMaxNumber {
@@ -14,5 +15,14 @@ public class FindMaxNumber {
                 .toList();
         System.out.println(res.getFirst()); //prints max number
         System.out.println(res.getLast()); //prints min number
+
+        //Alternate method
+        Optional<Integer> opt = nums.stream()
+                .max(Integer::compareTo);
+        System.out.println(opt.get());
+
+        Optional<Integer> opt2 = nums.stream()
+                .min(Integer::compareTo);
+        System.out.println(opt2.get());
     }
 }
