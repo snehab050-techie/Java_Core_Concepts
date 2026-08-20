26c027da2ab945f136a258516c857c34186524bb
snehab050-techie <snehab050@gmail.com>
2026-08-20 22:46:01 +0530

    Adding an alternative approach to find duplicate elements in a list using Collection - frequency method

--- Java_Core/src/Java_8_Features/interview_coding_questions/FindDuplicateElements.java
+++ Java_Core/src/Java_8_Features/interview_coding_questions/FindDuplicateElements.java
@@ -1,9 +1,6 @@
 package Java_8_Features.interview_coding_questions;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class FindDuplicateElements {
     static void main() {
@@ -12,12 +9,24 @@ public class FindDuplicateElements {
 
         Set<Integer> seen = new HashSet<>();
 
+        //method 1
        List<Integer> result = nums.stream()
                 .filter(n -> !seen.add(n)) // add() returns true if the set 'seen' did not already contain the specified element
                 .distinct()
                 .toList();
 
         System.out.println(result);
+
+        //method 2
+        List<Integer> res = nums.stream()
+                .filter(n->Collections.frequency(nums, n) > 1)
+                .distinct()
+                .toList();
+
+        //Collections.frequency(nums, n) returns the frequency of occurrence of n in list- nums
+
+        System.out.println("-----");
+        System.out.println(res);
 
     }
 }
